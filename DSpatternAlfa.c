377760fb1a48855c6e826e97e2708d3e5ca88f4c
codeBunny2022 <119339257+codeBunny2022@users.noreply.github.com>
2022-11-30 12:18:12 +0530

    ds upload

---
+++ DSpatternAlfa.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char d='A';
+    for(int i=1;i<=4;i++)
+    {
+        for(int j =0;j<i;j++)
+        {
+            printf("%c",d);
+            d++;
+        }
+        printf("\n");
+    }
+}
